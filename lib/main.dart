f3fc0af129e162bedcb9a77c64a3e5dde2ef4048
Chamith <chameeefdo@gmail.com>
2021-05-06 19:44:06 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+//the main function is the staring pont for all our flutter app
+void main() {
+  runApp(
+    MaterialApp(
+      home: Center(
+        child: Text('Hello chamith'),
+      ),
+    ),
+  );
+}

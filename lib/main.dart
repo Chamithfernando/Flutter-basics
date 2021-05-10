8e78dec3694c5c810f5bc7366d894a2ae3a04333
Chamith <chameeefdo@gmail.com>
2021-05-10 10:17:07 +0530

    Flutter card using card padding

--- lib/main.dart
+++ lib/main.dart
@@ -116,53 +116,59 @@ class MyApp extends StatelessWidget {
               ),
 
             ),
-                Container(
+                Card(
                   color: Colors.white,
                   margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget> [
-                      Icon(
-                        Icons.phone,
-                        color: Colors.teal,
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        '+94 77 231 5959',
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily:'SourceSansPro',
+                  
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget> [
+                        Icon(
+                          Icons.phone,
+                          color: Colors.teal,
                         ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          '+94 77 231 5959',
+                          style: TextStyle(
+                            color: Colors.teal.shade900,
+                            fontFamily:'SourceSansPro',
+                          ),
 
-                      ),
-                    ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                Container(
+                Card(
                   color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
-                  child: Row(
-                    children: <Widget> [
-                      Icon(
-                        Icons.email,
-                        color: Colors.teal,
 
-                      ),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        'Chameeefdo@gmail.com',
-                        style: TextStyle(
+                  margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Row(
+                      children: <Widget> [
+                        Icon(
+                          Icons.email,
                           color: Colors.teal,
-                          fontFamily: 'SourceSansPro',
 
                         ),
-                      ),
-                    ],
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Chameeefdo@gmail.com',
+                          style: TextStyle(
+                            color: Colors.teal,
+                            fontFamily: 'SourceSansPro',
+
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ],
